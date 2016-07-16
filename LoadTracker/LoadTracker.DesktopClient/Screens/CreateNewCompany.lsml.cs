a54529ee669316b5bdd2043cb467c63dba44a410
Tom Claasens <thomas.claasens@outlook.com>
2016-07-16 06:57:35 +0200

    initial commit

---
+++ LoadTracker/LoadTracker.DesktopClient/Screens/CreateNewCompany.lsml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.LightSwitch.Presentation.Extensions;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewCompany
+    {
+        partial void CreateNewCompany_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Write your code here.
+            this.CompanyProperty = new Company();
+        }
+
+        partial void CreateNewCompany_Saved()
+        {
+            // Write your code here.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.CompanyProperty);
+        }
+    }
+}
---
+++ LoadTracker/LoadTracker.HTMLClient/Screens/Towns/ViewTown.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewTown.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Town.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Town." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
