10dc08b8839e5a54d112db3e54205b57a0d8488b
Tom Claasens <thomas.claasens@outlook.com>
2016-07-16 08:55:52 +0200

    Screen layout for the Invoice capture

---
+++ LoadTracker/LoadTracker.DesktopClient/Screens/CreateNewInvoiceLine.lsml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.LightSwitch.Presentation.Extensions;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class CreateNewInvoiceLine
+    {
+        partial void CreateNewInvoiceLine_InitializeDataWorkspace(global::System.Collections.Generic.List<global::Microsoft.LightSwitch.IDataService> saveChangesTo)
+        {
+            // Write your code here.
+            this.InvoiceLineProperty = new InvoiceLine();
+        }
+
+        partial void CreateNewInvoiceLine_Saved()
+        {
+            // Write your code here.
+            this.Close(false);
+            Application.Current.ShowDefaultScreen(this.InvoiceLineProperty);
+        }
+    }
+}
